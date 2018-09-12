167bc7d6a0be30d6faaf1dafb4e805c91268b36b
snake0207 <snake0207@gmail.com>
2018-09-12 10:52:35 +0900

    init

---
+++ src/com/acro/servlet/DoLogin.java
@@ -0,0 +1,41 @@
+package com.acro.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DoLogin
+ */
+@WebServlet("/DoLogin")
+public class DoLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+        out.println("<HTML><HEAD><TITLE>DoLogin</TITLE></HEAD>");
+        out.println("<BODY>");
+        out.println("<H2> username : " + request.getParameter("username") + "</H2>");
+        out.println("<H2> password : " + request.getParameter("password") + "</H2>");
+        out.println("</BODY></HTML>");
+	}
+
+}
